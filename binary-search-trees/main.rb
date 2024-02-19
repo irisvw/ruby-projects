29db9d54943b5f69e88b86890361ca7ca2ea47d7
Iris van Willigen <iris.van.willigen@gmail.com>
2024-02-19 14:36:12 +0100

    add delete method

--- binary-search-trees/main.rb
+++ binary-search-trees/main.rb
@@ -3,8 +3,20 @@ require_relative 'tree.rb'
 
 array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 # array = [1, 7, 4, 23, 8]
-# array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# array = [1, 2, 3, 4, 5]
+# array = [1, 2]
 my_tree = Tree.new(array)
 my_tree.build_tree
 
-p my_tree.pretty_print
+my_tree.pretty_print
+
+# my_tree.insert(2)
+# my_tree.insert(7)
+# my_tree.pretty_print
+
+my_tree.delete(6345)
+my_tree.delete(5)
+my_tree.pretty_print
+
+my_tree.delete(8)
+my_tree.pretty_print
--- binary-search-trees/tree.rb
+++ binary-search-trees/tree.rb
@@ -11,7 +11,7 @@ class Tree
     return nil if start > finish
 
     # calculate mid point
-    mid = (start + finish) / 2 # 
+    mid = (start + finish) / 2
 
     # set middle element as root
     root = Node.new(array[mid])
@@ -21,17 +21,87 @@ class Tree
 
     # calculate mid of right_array, assign to right
     root.right = build_tree(array, mid+1, finish)
-    
+
     # return root as node
     return root
   end
 
-  def insert
+  def insert(value, root = @root)
     # accepts value to insert
+    # always as a leaf
+    # search for the parent key from the root
+
+    # base case
+    return Node.new(value) if root.nil?
+
+    # return existing value if value already exists
+    if root.data == value
+      return root
+    elsif root.data < value
+      root.right = insert(value, root.right)
+    else
+      root.left = insert(value, root.left)
+    end
+
+    return root
+    # currentnode.data == value ? return nil.
+    # currentnode.data < value ? currentnode = currentnode.right
+    # currentnode.data > value ? currentnode = currentnode.left
+    # if node.left.nil? && node.right.nil?
+    # if value > currentnode.data, currentnode.right = value
+    # else currentnode.left = value
   end
 
-  def delete
+  def delete(value, root = @root)
     # accepts value to delete
+    # recursively traverse the tree?
+
+    # base case
+    return root if root.nil?
+
+    if root.data < value
+      root.right = delete(value, root.right)
+      return root
+    elsif root.data > value
+      root.left = delete(value, root.left)
+      return root
+    end
+
+    # case 2: node with 1 kid
+    # replace node with its child
+
+    if root.left.nil?
+      root = root.right 
+      return root
+    elsif root.right.nil?
+      root = root.left
+      return root
+    else
+      # case 3: node with 2 kids
+      # find the smallest value in its right subtree
+      
+      parent_root = root
+      next_root = root.right
+
+      # next_root = next_root.left until next_root.left.nil?
+      while next_root.left != nil
+        parent_root = next_root
+        next_root = next_root.left
+      end
+
+      # if next_root has no kids, replace root with next_root and get rid of next_root.
+      # if next_root has a kid, it's always a kid with a larger value than itself.
+      # append kid to next_root's parent on the left side. 
+      root.data = next_root.data
+
+      if parent_root != root
+        parent_root.left = next_root.right
+      else
+        parent_root.right = next_root.right 
+      end
+
+      return root
+    end
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
